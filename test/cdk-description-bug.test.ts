99b43c927f8df3348204f9c9d3c2629a3f11c01e
Jacob Schooley <jacob@schooley.com>
2024-01-31 11:24:21 -0700

    Initial commit

---
+++ test/cdk-description-bug.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkDescriptionBug from '../lib/cdk-description-bug-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-description-bug-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkDescriptionBug.CdkDescriptionBugStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
